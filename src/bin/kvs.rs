8a6a8132e6b4da9bd5f2eb999fb58011e2aa5fda
Adam-Teng <656081024@qq.com>
2023-01-10 19:49:11 +0800

    finish project 1

---
+++ src/bin/kvs.rs
@@ -0,0 +1,39 @@
+use structopt::clap::{App, AppSettings, SubCommand};
+
+fn main() {
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(AppSettings::DisableHelpSubcommand)
+        .subcommands(vec![
+            SubCommand::with_name("set")
+                .about("Set the value of a string key to a string")
+                .args_from_usage(
+                    "<KEY> 'the key you want to set'
+                    <VALUE> 'the value you want to set to the key'",
+                ),
+            SubCommand::with_name("get")
+                .about("Get the string value of a given string key")
+                .arg_from_usage("<KEY> 'the key you want to look up'"),
+            SubCommand::with_name("rm")
+                .about("Remove a given key")
+                .arg_from_usage("<KEY> 'the key you want to remove'"),
+        ])
+        .get_matches();
+
+    match matches.subcommand() {
+        ("set", Some(_matches)) => {
+            panic!("unimplemented");
+        }
+        ("get", Some(_matches)) => {
+            panic!("unimplemented");
+        }
+        ("rm", Some(_matches)) => {
+            panic!("unimplemented");
+        }
+        _ => {
+            panic!("no args");
+        }
+    }
+}
